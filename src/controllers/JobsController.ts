4ec607c563ea9cc27c2f86eb2e1f034379cb93d2
Carlos Eduardo Mendes <carlosmendes@recife.ifpe.local>
2021-07-02 11:46:19 -0300

    added job creation logic and password check

--- src/controllers/JobsController.ts
+++ src/controllers/JobsController.ts
@@ -17,22 +17,40 @@ class JobsController {
             jobArea: area,
         } = request.body;
 
-        await db.run(`INSERT INTO jobs (
-            title,
-            description,
-            user_id,
-            area
-            ) VALUES (
-                '${title}',
-                '${description}',
-                ${user_id},
-                '${area}'
-            )
-        `);
 
-        await db.close();
+        const userPassword = await db.get(`SELECT * FROM users WHERE id = ${user_id}`);
 
-        return response.redirect('/');
+
+        if(userPassword == undefined){
+            return response.render('error', {message: 'Usuário não encontrado'});
+        } else {
+            if(userPassword.password == password){
+                await db.run(`INSERT INTO jobs (
+                    title,
+                    description,
+                    user_id,
+                    area
+                    ) VALUES (
+                        '${title}',
+                        '${description}',
+                        ${user_id},
+                        '${area}'
+                    )
+                `);
+
+                await db.close();
+
+                return response.redirect('/');
+            } else {
+                return response.render('error', {message: 'Senha incorreta. Tente novamente'});
+            }
+        }
+
+
+            
+
+
+                 
     }
 }
 
